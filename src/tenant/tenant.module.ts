8c50f626378792e6c1acc26cccda1911426a4b10
Robson Fernando Trasel de Souza <95061628+RobsonTrasel@users.noreply.github.com>
2022-08-03 00:46:31 -0300

    set tenant for catch subdomain/setup tenant model

--- src/tenant/tenant.module.ts
+++ src/tenant/tenant.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TenantService } from './tenant/tenant.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Account } from '../accounts/entities/account.entity';
 
 @Module({
+  imports: [
+    SequelizeModule.forFeature([Account])
+  ],
   providers: [TenantService]
 })
 export class TenantModule {}
--- src/tenant/tenant/tenant.service.ts
+++ src/tenant/tenant/tenant.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { Account } from '../../accounts/entities/account.entity';
 
 @Injectable()
-export class TenantService {}
+export class TenantService {
+
+    private account: Account | null = null
+    constructor(@InjectModel(Account) private accountModel: typeof Account) {}
+
+    get tenant() {
+        return this.account
+    }
+
+    set tenant(tenant: Account) {
+        this.account = tenant
+    }
+
+    async setTenantBy(subdomain: string) {
+        this.tenant = await this.accountModel.findOne({
+            where: {
+                subdomain,
+            },
+            rejectOnEmpty: true,
+        })
+    }
+
+}
